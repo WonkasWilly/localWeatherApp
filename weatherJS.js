fa8e27b13e9f7778ffa435439788cb9b3d3c8b94
Wonkaswilly <pchoi13370@gmail.com>
2017-07-31 15:56:10 -0700

    Almost finished with weather project!

--- weatherJS.js
+++ weatherJS.js
@@ -1,13 +1,55 @@
 $(function() {
    function success (position) {
-       console.log('GPS coordinates');
+
        var lat = position.coords.latitude;
        var long = position.coords.longitude;
-       console.log('Lat: ' + lat);
-       console.log('Long: ' + long);
+
+       $.ajax({
+           cache: false,
+           url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=89110c6d8ecedeaf507af6708c1cac08',
+           success: function(data) {
+               var kelvinTemp = data.main.temp;
+               var kelvinToFaren = Math.round(kelvinTemp * 9/5 - 459.67);
+               var weatherDesc = data.weather[0].main;
+               var location = data.name;
+               var farhen = '<i class="wi wi-fahrenheit"></i>';
+               var celc = '<i class="wi wi-celsius"></i>';
+               $('.temp').css({"font-size": "6em"});
+               $('#location').text(location);
+               $('#weather-desc').text(weatherDesc);
+               $('.temp').text(kelvinToFaren).append(farhen);
+           }
+
+
+       });
    }
+
    function error () {
-       alert('Something went wrong.');
+       alert('Sorry, it looks like something went wrong!');
    }
-   navigator.geolocation.getCurrentPosition(success, error);
+
+   function toCelc() {
+        var fahrenToCelc = ($('.temp').text() - 32) * 5/9;
+        $('.temp').text(fahrenToCelc).append("<i class='wi wi-celsius'></i>");
+    }
+    function toFahren() {
+       var celcToFahren = $('.temp').text * 9/5 + 32;
+
+    }
+
+    var currentUnit = 'Fahrenheit';
+   $('#weather-button').click(function(){
+       navigator.geolocation.getCurrentPosition(success, error);
+   });
+   $('.temp').click(function() {
+       if (currentUnit === 'Fahrenheit') {
+           toCelc();
+       }
+       else {
+           toFahren();
+           currentUnit = 'Celcius';
+       }
+
+   });
+
 });
